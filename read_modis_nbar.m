feff8f3b65a8ec83043049cf896bff8e039bc8ea
mpdannenberg <matthew-dannenberg@uiowa.edu>
2022-01-27 16:02:27 -0600

    Remove outlier filter

--- read_modis_nbar.m
+++ read_modis_nbar.m
@@ -1,4 +1,4 @@
-% Read in MODIS LST
+% Read in MODIS vegetation indices
 
 load ./data/Ameriflux_daily.mat;
 n = length(Ameriflux);
@@ -42,14 +42,14 @@ for i = 1:n
     LSWI2 = (Band2 - Band6) ./ (Band2 + Band6);
     LSWI3 = (Band2 - Band7) ./ (Band2 + Band7);
     
-    % Filter vegetation indices for outliers 
-    NDVI_f = filtering_outliers(NDVI, doy, 'lwl',3, 'uwl',3, 'thres',0.1*range(NDVI), 'cycles',2, 'winsize',15);
-    kNDVI_f = filtering_outliers(kNDVI, doy, 'lwl',3, 'uwl',3, 'thres',0.1*range(kNDVI), 'cycles',2, 'winsize',15);
-    EVI_f = filtering_outliers(EVI, doy, 'lwl',3, 'uwl',3, 'thres',0.1*range(EVI), 'cycles',2, 'winsize',15);
-    NIRv_f = filtering_outliers(NIRv, doy, 'lwl',3, 'uwl',3, 'thres',0.1*range(NIRv), 'cycles',2, 'winsize',15);
-    LSWI1_f = filtering_outliers(LSWI1, doy, 'lwl',3, 'uwl',3, 'thres',0.1*range(LSWI1), 'cycles',2, 'winsize',15);
-    LSWI2_f = filtering_outliers(LSWI2, doy, 'lwl',3, 'uwl',3, 'thres',0.1*range(LSWI2), 'cycles',2, 'winsize',15);
-    LSWI3_f = filtering_outliers(LSWI3, doy, 'lwl',3, 'uwl',3, 'thres',0.1*range(LSWI3), 'cycles',2, 'winsize',15);
+%     % Filter vegetation indices for outliers 
+%     NDVI_f = filtering_outliers(NDVI, doy, 'lwl',3, 'uwl',3, 'thres',0.1*range(NDVI), 'cycles',2, 'winsize',15);
+%     kNDVI_f = filtering_outliers(kNDVI, doy, 'lwl',3, 'uwl',3, 'thres',0.1*range(kNDVI), 'cycles',2, 'winsize',15);
+%     EVI_f = filtering_outliers(EVI, doy, 'lwl',3, 'uwl',3, 'thres',0.1*range(EVI), 'cycles',2, 'winsize',15);
+%     NIRv_f = filtering_outliers(NIRv, doy, 'lwl',3, 'uwl',3, 'thres',0.1*range(NIRv), 'cycles',2, 'winsize',15);
+%     LSWI1_f = filtering_outliers(LSWI1, doy, 'lwl',3, 'uwl',3, 'thres',0.1*range(LSWI1), 'cycles',2, 'winsize',15);
+%     LSWI2_f = filtering_outliers(LSWI2, doy, 'lwl',3, 'uwl',3, 'thres',0.1*range(LSWI2), 'cycles',2, 'winsize',15);
+%     LSWI3_f = filtering_outliers(LSWI3, doy, 'lwl',3, 'uwl',3, 'thres',0.1*range(LSWI3), 'cycles',2, 'winsize',15);
     
     Ameriflux(i).NDVI = NaN(size(dt));
     Ameriflux(i).EVI = NaN(size(dt));
@@ -59,13 +59,13 @@ for i = 1:n
     Ameriflux(i).LSWI2 = NaN(size(dt));
     Ameriflux(i).LSWI3 = NaN(size(dt));
     
-    Ameriflux(i).NDVI(ia) = NDVI_f(ib);
-    Ameriflux(i).EVI(ia) = EVI_f(ib); 
-    Ameriflux(i).NIRv(ia) = NIRv_f(ib);
-    Ameriflux(i).kNDVI(ia) = kNDVI_f(ib);
-    Ameriflux(i).LSWI1(ia) = LSWI1_f(ib);
-    Ameriflux(i).LSWI2(ia) = LSWI2_f(ib);
-    Ameriflux(i).LSWI3(ia) = LSWI3_f(ib);
+    Ameriflux(i).NDVI(ia) = NDVI(ib);
+    Ameriflux(i).EVI(ia) = EVI(ib); 
+    Ameriflux(i).NIRv(ia) = NIRv(ib);
+    Ameriflux(i).kNDVI(ia) = kNDVI(ib);
+    Ameriflux(i).LSWI1(ia) = LSWI1(ib);
+    Ameriflux(i).LSWI2(ia) = LSWI2(ib);
+    Ameriflux(i).LSWI3(ia) = LSWI3(ib);
     
     
 end
