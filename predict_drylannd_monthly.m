5173bcdcd8c85803d05e57183d4c7dbb4ca371e0
mpdannenberg <matthew-dannenberg@uiowa.edu>
2022-01-28 10:31:22 -0600

    Initial commit

---
+++ predict_drylannd_monthly.m
@@ -0,0 +1,91 @@
+% Predict GPP, NEE, and ET at gridded scale
+
+% Parameters
+ai_threshold = 0.75;
+credInt = [0.1 0.9];
+
+% Load model
+load ./output/DrylANNd_monthly.mat;
+nets = DrylANNd.NNets;
+nsims = length(nets);
+yscale = DrylANNd.Yscale;
+yoffset = DrylANNd.Yoffset;
+
+% Load Aridity data
+load ./data/TerraClimate_AridityIndex.mat;
+
+% Load predictor data
+load ./data/SMAP_monthly.mat;
+load ./data/LST_monthly.mat;
+load ./data/NBAR_monthly.mat;
+load ./data/MCD12C1_fc.mat;
+clear MCD12C1_WAT MCD12C1_CRP MCD12C1_BSV;
+[nt, ny, nx] = size(SMAP_RZSM_monthly);
+
+% Create empty arrays for response variables
+GPP = NaN(nt, ny, nx);
+GPP_low = NaN(nt, ny, nx);
+GPP_high = NaN(nt, ny, nx);
+NEE = NaN(nt, ny, nx);
+NEE_low = NaN(nt, ny, nx);
+NEE_high = NaN(nt, ny, nx);
+ET = NaN(nt, ny, nx);
+ET_low = NaN(nt, ny, nx);
+ET_high = NaN(nt, ny, nx);
+
+% Loop through each time step and predict
+for t = 1:nt
+    
+    % Arrange predictor data
+    X = [MCD43_NDVI_monthly(t, ai <= ai_threshold)
+        MCD43_EVI_monthly(t, ai <= ai_threshold)
+        MCD43_NIRv_monthly(t, ai <= ai_threshold)
+        MCD43_kNDVI_monthly(t, ai <= ai_threshold)
+        MCD43_LSWI1_monthly(t, ai <= ai_threshold)
+        MCD43_LSWI2_monthly(t, ai <= ai_threshold)
+        MCD43_LSWI3_monthly(t, ai <= ai_threshold)
+        MOD11_Day_monthly(t, ai <= ai_threshold)
+        MOD11_Night_monthly(t, ai <= ai_threshold)
+        MYD11_Day_monthly(t, ai <= ai_threshold)
+        MYD11_Night_monthly(t, ai <= ai_threshold)
+        SMAP_RZSM_monthly(t, ai <= ai_threshold)
+        SMAP_SFSM_monthly(t, ai <= ai_threshold)
+        SMAP_TS_monthly(t, ai <= ai_threshold)
+        MCD12C1_FOR(ai <= ai_threshold)'
+        MCD12C1_GRS(ai <= ai_threshold)'
+        MCD12C1_SAV(ai <= ai_threshold)'
+        MCD12C1_SHB(ai <= ai_threshold)'];
+
+    Y = NaN(length(yscale), size(X,2), nsims);
+    Ys = repmat(yscale, 1, size(X,2));
+    Ym = repmat(yoffset, 1, size(X,2));
+    
+    for i = 1:nsims
+
+        net = nets{i};
+        Y(:,:,i) = net(X) .* Ys + Ym;
+
+    end
+
+    Y_ens = median(Y, 3);
+    Y_ens_low = quantile(Y, credInt(1), 3);
+    Y_ens_high = quantile(Y, credInt(2), 3);
+    
+    GPP(t, ai <= ai_threshold) = Y_ens(1,:);
+    NEE(t, ai <= ai_threshold) = Y_ens(2,:);
+    ET(t, ai <= ai_threshold) = Y_ens(3,:);
+    
+    GPP_low(t, ai <= ai_threshold) = Y_ens_low(1,:);
+    NEE_low(t, ai <= ai_threshold) = Y_ens_low(2,:);
+    ET_low(t, ai <= ai_threshold) = Y_ens_low(3,:);
+    
+    GPP_high(t, ai <= ai_threshold) = Y_ens_high(1,:);
+    NEE_high(t, ai <= ai_threshold) = Y_ens_high(2,:);
+    ET_high(t, ai <= ai_threshold) = Y_ens_high(3,:);
+    
+end
+
+save('./output/DrylANNd_monthly_prediction.mat', 'GPP','ET','NEE',...
+    'GPP_high','ET_high','NEE_high','GPP_low','ET_low','NEE_low',...
+    'lat','lon','yr','doy', '-v7.3');
+
