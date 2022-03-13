e30c507a185034edfea6b134fd52eb91fdaff031
mpdannenberg <matthew-dannenberg@uiowa.edu>
2022-03-13 15:37:01 -0500

    Update figure, clean up/correct some code, switch to relative change in MAE, and exclude sites that weren't used from calculation

--- calculate_variable_importance_8day.m
+++ calculate_variable_importance_8day.m
@@ -7,10 +7,15 @@ vars = {'NDVI','EVI','NIRv','kNDVI',...
     'L4SM_Root','L4SM_Surf','L4SM_TSoil'};
 ks = length(vars);
 rng(3);
+excludeSites = {'MX-EMg','US-CZ4','US-Me2','US-Sne','US-Snf'}; % exclude sites from calibration (not dryland, missing data, or weird site)
+stagger = 0.08;
 
 load ./data/Ameriflux_8day;
 load ./output/DrylANNd_8day.mat;
+Ameriflux_8day = Ameriflux_8day(~ismember({Ameriflux_8day.Site}, excludeSites));
 n = length(Ameriflux_8day);
+yscale = DrylANNd.Yscale;
+yoffset = DrylANNd.Yoffset;
 
 dMAE_GPP = NaN(ks,n);
 dMAE_NEE = NaN(ks,n);
@@ -19,28 +24,31 @@ dMAE_ET = NaN(ks,n);
 % Loop through each site and calculate change in MAE when variable is
 % destroyed
 for i = 1:n
-    % Convert monthly mean latent heat flux to monthly mean daily ET
+    % Convert 8day mean latent heat flux to 8day mean daily ET
     Ameriflux_8day(i).ET = Ameriflux_8day(i).LE * (1/1000000) * 60 * 60 * 24 * 0.408; % W m-2 --> MJ m-2 day-2 --> mm day-1
     C = Ameriflux_8day(i);
     Xc = [extractfield(C, 'NDVI')' extractfield(C, 'EVI')' extractfield(C, 'NIRv')' ...
         extractfield(C, 'kNDVI')' extractfield(C, 'LSWI1')' extractfield(C, 'LSWI2')' extractfield(C, 'LSWI3')' ...
         extractfield(C, 'MOD11_Day')' extractfield(C, 'MOD11_Night')' extractfield(C, 'MYD11_Day')' extractfield(C, 'MYD11_Night')' ...
         extractfield(C, 'L4SM_Root')' extractfield(C, 'L4SM_Surf')' extractfield(C, 'L4SM_Tsoil')' ...
-        extractfield(C, 'MCD12_FOR')' extractfield(C, 'MCD12_GRS')' extractfield(C, 'MCD12_SAV')' extractfield(C, 'MCD12_SHB')']'; % Add more as needed
+        extractfield(C, 'Rangeland_AFG')' extractfield(C, 'Rangeland_PFG')' extractfield(C, 'Rangeland_SHR')' extractfield(C, 'Rangeland_TRE')' extractfield(C, 'Rangeland_LTR')' extractfield(C, 'Rangeland_BGR')']'; % Add more as needed
     Yc = [extractfield(C, 'GPP')' extractfield(C, 'NEE')' extractfield(C, 'ET')']'; 
     
     % Initialize validation matrix
     Yv = NaN(size(Yc,1), size(Xc,2), nsims);
     Yvv = NaN(size(Yc,1), size(Xc,2), nsims);
     
+    Ys = repmat(yscale, 1, size(Xc,2));
+    Ym = repmat(yoffset, 1, size(Xc,2));
+
     nets = DrylANNd.NNets;
     
     for k = 1:ks
         
         kidx = find(strcmp(vars(k), DrylANNd.Xnames));
 
-        mu = nanmean(Xc(kidx,:));
-        s = nanstd(Xc(kidx,:));
+        mu = mean(Xc(kidx,:), 'omitnan');
+        s = std(Xc(kidx,:), 'omitnan');
         
         for j = 1:nsims
             
@@ -48,16 +56,16 @@ for i = 1:n
             Xcc(kidx, :) = normrnd(mu, s, [1, size(Xc, 2)]);
             
             net = nets{randi(length(nets), 1)};
-            Yvv(:,:,j) = net(Xcc) .* DrylANNd.Yscale + DrylANNd.Yoffset;
-            Yv(:,:,j) = net(Xc) .* DrylANNd.Yscale + DrylANNd.Yoffset;
+            Yvv(:,:,j) = net(Xcc) .* Ys + Ym;
+            Yv(:,:,j) = net(Xc) .* Ys + Ym;
 
         end
         Yv_mean = squeeze(nanmean(Yv(:,:,:), 3));
         Yvv_mean = squeeze(nanmean(Yvv(:,:,:), 3));
         
-        dMAE_GPP(k, i) = nanmean(abs(Yvv_mean(1,:) - Yc(1,:))) - nanmean(abs(Yv_mean(1,:) - Yc(1,:)));
-        dMAE_NEE(k, i) = nanmean(abs(Yvv_mean(2,:) - Yc(2,:))) - nanmean(abs(Yv_mean(2,:) - Yc(2,:)));
-        dMAE_ET(k, i) = nanmean(abs(Yvv_mean(3,:) - Yc(3,:))) - nanmean(abs(Yv_mean(3,:) - Yc(3,:)));
+        dMAE_GPP(k, i) = 100 * (nanmean(abs(Yvv_mean(1,:) - Yc(1,:))) - nanmean(abs(Yv_mean(1,:) - Yc(1,:)))) ./ nanmean(abs(Yv_mean(1,:) - Yc(1,:)));
+        dMAE_NEE(k, i) = 100 * (nanmean(abs(Yvv_mean(2,:) - Yc(2,:))) - nanmean(abs(Yv_mean(2,:) - Yc(2,:)))) ./ nanmean(abs(Yv_mean(2,:) - Yc(2,:)));
+        dMAE_ET(k, i) = 100 * (nanmean(abs(Yvv_mean(3,:) - Yc(3,:))) - nanmean(abs(Yv_mean(3,:) - Yc(3,:)))) ./ nanmean(abs(Yv_mean(3,:) - Yc(3,:)));
         
     end
 end
@@ -76,88 +84,164 @@ clr = wesanderson('aquatic4'); clr(3,:) = [];
 
 h = figure('Color','w');
 h.Units = 'inches';
-h.Position = [1 1 5 4];
+h.Position = [1 1 3.5 8];
 
+axf = tight_subplot(3, 1, 0.08, [0.1 0.04], [0.1 0.04]);
+
+% GPP
+xlim = [-25 175];
+axes(axf(1))
 ym = nanmedian(dMAE_GPP, 2);
 [~,idx] = sort(ym,'ascend');
 
 for i = 1:length(ulc)
     lcidx = find(strcmp(lc, ulc{i}));
     dmae = dMAE_GPP(idx, lcidx);
-    scatter(reshape(dmae,[],1), reshape(repmat(1:ks, length(lcidx), 1)', [],1), 10, clr(i,:), 'filled')
+    scatter(reshape(dmae,[],1), reshape(repmat(1:ks, length(lcidx), 1)'+normrnd(0,stagger,size(dmae)), [],1), 10, clr(i,:), 'filled')
     hold on;
 end
 scatter(ym(idx), 1:ks, 40, 'k', 'Marker','|', 'LineWidth',1.5)
-xlabel('\DeltaMAE (g C m^{-2} day^{-1})')
+xlabel('\DeltaMAE (%)')
 ax = gca;
 box off;
-set(ax, 'TickDir','out', 'TickLength',[0.025 0],'YColor','w','FontSize',9)
-ax.Position(1) = 0.22;
-ax.Position(3) = 0.75;
-text(repmat(ax.XLim(1),1,ks), 1:ks, varlabels(idx), 'HorizontalAlignment','right','FontSize',9)
-lgd = legend('ENF','GRA','SAV','SHB','Median', 'Location','southeast', 'FontSize',9);
+set(ax, 'TickDir','out', 'TickLength',[0.025 0],'YColor','w','FontSize',9, 'XLim',xlim)
+ax.Position(1) = 0.28;
+ax.Position(3) = 0.7;
+text(repmat(ax.XLim(1),1,ks), 1:ks, varlabels(idx), 'HorizontalAlignment','right','FontSize',8)
+lgd = legend('ENF','GRS','SAV','SHB', 'Location','northoutside', 'FontSize',7, 'Orientation','horizontal');
+lgd.Position(2) = 0.97;
 legend('boxoff')
+text(xlim(2),1,'a', 'FontSize',12, 'HorizontalAlignment','right')
 
-set(gcf,'PaperPositionMode','auto','InvertHardCopy','off')
-print('-dtiff','-f1','-r300','./output/DrylANNd_variableimportance_GPP_8day.tif')
-close all;
-
-% Plot NEE variable importance
-h = figure('Color','w');
-h.Units = 'inches';
-h.Position = [1 1 5 4];
-
+% NEE
+xlim = [-25 175];
+axes(axf(2))
 ym = nanmedian(dMAE_NEE, 2);
 [~,idx] = sort(ym,'ascend');
 
 for i = 1:length(ulc)
     lcidx = find(strcmp(lc, ulc{i}));
     dmae = dMAE_NEE(idx, lcidx);
-    scatter(reshape(dmae,[],1), reshape(repmat(1:ks, length(lcidx), 1)', [],1), 10, clr(i,:), 'filled')
+    scatter(reshape(dmae,[],1), reshape(repmat(1:ks, length(lcidx), 1)'+normrnd(0,stagger,size(dmae)), [],1), 10, clr(i,:), 'filled')
     hold on;
 end
 scatter(ym(idx), 1:ks, 40, 'k', 'Marker','|', 'LineWidth',1.5)
-xlabel('\DeltaMAE (g C m^{-2} day^{-1})')
+xlabel('\DeltaMAE (%)')
 ax = gca;
 box off;
-set(ax, 'TickDir','out', 'TickLength',[0.025 0],'YColor','w','FontSize',9)
-ax.Position(1) = 0.22;
-ax.Position(3) = 0.75;
-text(repmat(ax.XLim(1),1,ks), 1:ks, varlabels(idx), 'HorizontalAlignment','right','FontSize',9)
-lgd = legend('ENF','GRA','SAV','SHB','Median', 'Location','southeast', 'FontSize',9);
-legend('boxoff')
+set(ax, 'TickDir','out', 'TickLength',[0.025 0],'YColor','w','FontSize',9,'XLim',xlim)
+ax.Position(1) = 0.28;
+ax.Position(3) = 0.7;
+text(repmat(ax.XLim(1),1,ks), 1:ks, varlabels(idx), 'HorizontalAlignment','right','FontSize',8)
+text(xlim(2),1,'b', 'FontSize',12, 'HorizontalAlignment','right')
 
-set(gcf,'PaperPositionMode','auto','InvertHardCopy','off')
-print('-dtiff','-f1','-r300','./output/DrylANNd_variableimportance_NEE_8day.tif')
-close all;
-
-% Plot ET variable importance
-h = figure('Color','w');
-h.Units = 'inches';
-h.Position = [1 1 5 4];
-
+% ET
+xlim = [-25 175];
+axes(axf(3))
 ym = nanmedian(dMAE_ET, 2);
 [~,idx] = sort(ym,'ascend');
 
 for i = 1:length(ulc)
     lcidx = find(strcmp(lc, ulc{i}));
     dmae = dMAE_ET(idx, lcidx);
-    scatter(reshape(dmae,[],1), reshape(repmat(1:ks, length(lcidx), 1)', [],1), 10, clr(i,:), 'filled')
+    scatter(reshape(dmae,[],1), reshape(repmat(1:ks, length(lcidx), 1)'+normrnd(0,stagger,size(dmae)), [],1), 10, clr(i,:), 'filled')
     hold on;
 end
 scatter(ym(idx), 1:ks, 40, 'k', 'Marker','|', 'LineWidth',1.5)
-xlabel('\DeltaMAE (mm day^{-1})')
+xlabel('\DeltaMAE (%)')
 ax = gca;
 box off;
-set(ax, 'TickDir','out', 'TickLength',[0.025 0],'YColor','w','FontSize',9)
-ax.Position(1) = 0.22;
-ax.Position(3) = 0.75;
-text(repmat(ax.XLim(1),1,ks), 1:ks, varlabels(idx), 'HorizontalAlignment','right','FontSize',9)
-lgd = legend('ENF','GRA','SAV','SHB','Median', 'Location','southeast', 'FontSize',9);
-legend('boxoff')
+set(ax, 'TickDir','out', 'TickLength',[0.025 0],'YColor','w','FontSize',9,'XLim',xlim)
+ax.Position(1) = 0.28;
+ax.Position(3) = 0.7;
+text(repmat(ax.XLim(1),1,ks), 1:ks, varlabels(idx), 'HorizontalAlignment','right','FontSize',8)
+text(xlim(2),1,'c', 'FontSize',12, 'HorizontalAlignment','right')
 
 set(gcf,'PaperPositionMode','auto','InvertHardCopy','off')
-print('-dtiff','-f1','-r300','./output/DrylANNd_variableimportance_ET_8day.tif')
+print('-dtiff','-f1','-r300','./output/DrylANNd_variableimportance_8day.tif')
+close all;
+
+%% Presentation figure
+h = figure('Color','k');
+h.Units = 'inches';
+h.Position = [1 1 3.5 8];
+
+axf = tight_subplot(3, 1, 0.08, [0.1 0.04], [0.1 0.04]);
+
+% GPP
+xlim = [-25 175];
+axes(axf(1))
+ym = nanmedian(dMAE_GPP, 2);
+[~,idx] = sort(ym,'ascend');
+
+for i = 1:length(ulc)
+    lcidx = find(strcmp(lc, ulc{i}));
+    dmae = dMAE_GPP(idx, lcidx);
+    scatter(reshape(dmae,[],1), reshape(repmat(1:ks, length(lcidx), 1)'+normrnd(0,stagger,size(dmae)), [],1), 10, clr(i,:), 'filled')
+    hold on;
+end
+scatter(ym(idx), 1:ks, 40, 'w', 'Marker','|', 'LineWidth',1.5)
+xlabel('\DeltaMAE (%)', 'Color','w')
+ax = gca;
+box off;
+set(ax, 'TickDir','out', 'TickLength',[0.025 0],'FontSize',9,...
+    'XLim',xlim, 'Color','k', 'XColor','w', 'YColor','k')
+ax.Position(1) = 0.28;
+ax.Position(3) = 0.7;
+text(repmat(ax.XLim(1),1,ks), 1:ks, varlabels(idx), 'HorizontalAlignment','right','FontSize',8,'Color','w')
+lgd = legend('ENF','GRS','SAV','SHB', 'Location','northoutside', 'FontSize',7, 'Orientation','horizontal');
+lgd.Position(2) = 0.97;
+legend('boxoff')
+text(xlim(2),1,'a', 'FontSize',12, 'HorizontalAlignment','right')
+
+% NEE
+xlim = [-25 175];
+axes(axf(2))
+ym = nanmedian(dMAE_NEE, 2);
+[~,idx] = sort(ym,'ascend');
+
+for i = 1:length(ulc)
+    lcidx = find(strcmp(lc, ulc{i}));
+    dmae = dMAE_NEE(idx, lcidx);
+    scatter(reshape(dmae,[],1), reshape(repmat(1:ks, length(lcidx), 1)'+normrnd(0,stagger,size(dmae)), [],1), 10, clr(i,:), 'filled')
+    hold on;
+end
+scatter(ym(idx), 1:ks, 40, 'w', 'Marker','|', 'LineWidth',1.5)
+xlabel('\DeltaMAE (%)', 'Color','w')
+ax = gca;
+box off;
+set(ax, 'TickDir','out', 'TickLength',[0.025 0],'YColor','k','FontSize',9,...
+    'XLim',xlim, 'Color','k', 'XColor','w')
+ax.Position(1) = 0.28;
+ax.Position(3) = 0.7;
+text(repmat(ax.XLim(1),1,ks), 1:ks, varlabels(idx), 'HorizontalAlignment','right','FontSize',8,'Color','w')
+text(xlim(2),1,'b', 'FontSize',12, 'HorizontalAlignment','right')
+
+% ET
+xlim = [-25 175];
+axes(axf(3))
+ym = nanmedian(dMAE_ET, 2);
+[~,idx] = sort(ym,'ascend');
+
+for i = 1:length(ulc)
+    lcidx = find(strcmp(lc, ulc{i}));
+    dmae = dMAE_ET(idx, lcidx);
+    scatter(reshape(dmae,[],1), reshape(repmat(1:ks, length(lcidx), 1)'+normrnd(0,stagger,size(dmae)), [],1), 10, clr(i,:), 'filled')
+    hold on;
+end
+scatter(ym(idx), 1:ks, 40, 'w', 'Marker','|', 'LineWidth',1.5)
+xlabel('\DeltaMAE (%)', 'Color','w')
+ax = gca;
+box off;
+set(ax, 'TickDir','out', 'TickLength',[0.025 0],'YColor','k','FontSize',9,...
+    'XLim',xlim, 'Color','k', 'XColor','w')
+ax.Position(1) = 0.28;
+ax.Position(3) = 0.7;
+text(repmat(ax.XLim(1),1,ks), 1:ks, varlabels(idx), 'HorizontalAlignment','right','FontSize',8,'Color','w')
+text(xlim(2),1,'c', 'FontSize',12, 'HorizontalAlignment','right')
+
+set(gcf,'PaperPositionMode','auto','InvertHardCopy','off')
+print('-dtiff','-f1','-r300','./presentation/DrylANNd_variableimportance_8day.tif')
 close all;
 
 
--- calculate_variable_importance_monthly.m
+++ calculate_variable_importance_monthly.m
@@ -7,10 +7,15 @@ vars = {'NDVI','EVI','NIRv','kNDVI',...
     'L4SM_Root','L4SM_Surf','L4SM_TSoil'};
 ks = length(vars);
 rng(3);
+excludeSites = {'MX-EMg','US-CZ4','US-Me2','US-Sne','US-Snf'}; % exclude sites from calibration (not dryland, missing data, or weird site)
+stagger = 0.08;
 
 load ./data/Ameriflux_monthly;
 load ./output/DrylANNd_monthly.mat;
+Ameriflux_monthly = Ameriflux_monthly(~ismember({Ameriflux_monthly.Site}, excludeSites));
 n = length(Ameriflux_monthly);
+yscale = DrylANNd.Yscale;
+yoffset = DrylANNd.Yoffset;
 
 dMAE_GPP = NaN(ks,n);
 dMAE_NEE = NaN(ks,n);
@@ -26,21 +31,24 @@ for i = 1:n
         extractfield(C, 'kNDVI')' extractfield(C, 'LSWI1')' extractfield(C, 'LSWI2')' extractfield(C, 'LSWI3')' ...
         extractfield(C, 'MOD11_Day')' extractfield(C, 'MOD11_Night')' extractfield(C, 'MYD11_Day')' extractfield(C, 'MYD11_Night')' ...
         extractfield(C, 'L4SM_Root')' extractfield(C, 'L4SM_Surf')' extractfield(C, 'L4SM_Tsoil')' ...
-        extractfield(C, 'MCD12_FOR')' extractfield(C, 'MCD12_GRS')' extractfield(C, 'MCD12_SAV')' extractfield(C, 'MCD12_SHB')']'; % Add more as needed
+        extractfield(C, 'Rangeland_AFG')' extractfield(C, 'Rangeland_PFG')' extractfield(C, 'Rangeland_SHR')' extractfield(C, 'Rangeland_TRE')' extractfield(C, 'Rangeland_LTR')' extractfield(C, 'Rangeland_BGR')']'; % Add more as needed
     Yc = [extractfield(C, 'GPP')' extractfield(C, 'NEE')' extractfield(C, 'ET')']'; 
     
     % Initialize validation matrix
     Yv = NaN(size(Yc,1), size(Xc,2), nsims);
     Yvv = NaN(size(Yc,1), size(Xc,2), nsims);
-    
+
+    Ys = repmat(yscale, 1, size(Xc,2));
+    Ym = repmat(yoffset, 1, size(Xc,2));
+
     nets = DrylANNd.NNets;
     
     for k = 1:ks
         
         kidx = find(strcmp(vars(k), DrylANNd.Xnames));
 
-        mu = nanmean(Xc(kidx,:));
-        s = nanstd(Xc(kidx,:));
+        mu = mean(Xc(kidx,:), 'omitnan');
+        s = std(Xc(kidx,:), 'omitnan');
         
         for j = 1:nsims
             
@@ -48,22 +56,22 @@ for i = 1:n
             Xcc(kidx, :) = normrnd(mu, s, [1, size(Xc, 2)]);
             
             net = nets{randi(length(nets), 1)};
-            Yvv(:,:,j) = net(Xcc) .* DrylANNd.Yscale + DrylANNd.Yoffset;
-            Yv(:,:,j) = net(Xc) .* DrylANNd.Yscale + DrylANNd.Yoffset;
+            Yvv(:,:,j) = net(Xcc) .* Ys + Ym;
+            Yv(:,:,j) = net(Xc) .* Ys + Ym;
 
         end
         Yv_mean = squeeze(nanmean(Yv(:,:,:), 3));
         Yvv_mean = squeeze(nanmean(Yvv(:,:,:), 3));
         
-        dMAE_GPP(k, i) = nanmean(abs(Yvv_mean(1,:) - Yc(1,:))) - nanmean(abs(Yv_mean(1,:) - Yc(1,:)));
-        dMAE_NEE(k, i) = nanmean(abs(Yvv_mean(2,:) - Yc(2,:))) - nanmean(abs(Yv_mean(2,:) - Yc(2,:)));
-        dMAE_ET(k, i) = nanmean(abs(Yvv_mean(3,:) - Yc(3,:))) - nanmean(abs(Yv_mean(3,:) - Yc(3,:)));
+        dMAE_GPP(k, i) = 100 * (nanmean(abs(Yvv_mean(1,:) - Yc(1,:))) - nanmean(abs(Yv_mean(1,:) - Yc(1,:)))) ./ nanmean(abs(Yv_mean(1,:) - Yc(1,:)));
+        dMAE_NEE(k, i) = 100 * (nanmean(abs(Yvv_mean(2,:) - Yc(2,:))) - nanmean(abs(Yv_mean(2,:) - Yc(2,:)))) ./ nanmean(abs(Yv_mean(2,:) - Yc(2,:)));
+        dMAE_ET(k, i) = 100 * (nanmean(abs(Yvv_mean(3,:) - Yc(3,:))) - nanmean(abs(Yv_mean(3,:) - Yc(3,:)))) ./ nanmean(abs(Yv_mean(3,:) - Yc(3,:)));
         
     end
 end
 toc
 
-% Plot GPP variable importance
+% Plot variable importance
 varlabels = {'NDVI','EVI','NIRv','kNDVI','LSWI (band 5)','LSWI (band 6)','LSWI (band 7)',...
     'MOD11 (Day)','MOD11 (Night)','MYD11 (Day)','MYD11 (Night)',...
     'L4SM (rootzone)','L4SM (surface)','L4SM (T_{soil})'};
@@ -76,88 +84,164 @@ clr = wesanderson('aquatic4'); clr(3,:) = [];
 
 h = figure('Color','w');
 h.Units = 'inches';
-h.Position = [1 1 5 4];
+h.Position = [1 1 3.5 8];
 
+axf = tight_subplot(3, 1, 0.08, [0.1 0.04], [0.1 0.04]);
+
+% GPP
+xlim = [-25 175];
+axes(axf(1))
 ym = nanmedian(dMAE_GPP, 2);
 [~,idx] = sort(ym,'ascend');
 
 for i = 1:length(ulc)
     lcidx = find(strcmp(lc, ulc{i}));
     dmae = dMAE_GPP(idx, lcidx);
-    scatter(reshape(dmae,[],1), reshape(repmat(1:ks, length(lcidx), 1)', [],1), 10, clr(i,:), 'filled')
+    scatter(reshape(dmae,[],1), reshape(repmat(1:ks, length(lcidx), 1)'+normrnd(0,stagger,size(dmae)), [],1), 10, clr(i,:), 'filled')
     hold on;
 end
 scatter(ym(idx), 1:ks, 40, 'k', 'Marker','|', 'LineWidth',1.5)
-xlabel('\DeltaMAE (g C m^{-2} day^{-1})')
+xlabel('\DeltaMAE (%)')
 ax = gca;
 box off;
-set(ax, 'TickDir','out', 'TickLength',[0.025 0],'YColor','w','FontSize',9)
-ax.Position(1) = 0.22;
-ax.Position(3) = 0.75;
-text(repmat(ax.XLim(1),1,ks), 1:ks, varlabels(idx), 'HorizontalAlignment','right','FontSize',9)
-lgd = legend('ENF','GRA','SAV','SHB','Median', 'Location','southeast', 'FontSize',9);
+set(ax, 'TickDir','out', 'TickLength',[0.025 0],'YColor','w','FontSize',9, 'XLim',xlim)
+ax.Position(1) = 0.28;
+ax.Position(3) = 0.7;
+text(repmat(ax.XLim(1),1,ks), 1:ks, varlabels(idx), 'HorizontalAlignment','right','FontSize',8)
+lgd = legend('ENF','GRS','SAV','SHB', 'Location','northoutside', 'FontSize',7, 'Orientation','horizontal');
+lgd.Position(2) = 0.97;
 legend('boxoff')
+text(xlim(2),1,'a', 'FontSize',12, 'HorizontalAlignment','right')
 
-set(gcf,'PaperPositionMode','auto','InvertHardCopy','off')
-print('-dtiff','-f1','-r300','./output/DrylANNd_variableimportance_GPP_monthly.tif')
-close all;
-
-% Plot NEE variable importance
-h = figure('Color','w');
-h.Units = 'inches';
-h.Position = [1 1 5 4];
-
+% NEE
+xlim = [-25 175];
+axes(axf(2))
 ym = nanmedian(dMAE_NEE, 2);
 [~,idx] = sort(ym,'ascend');
 
 for i = 1:length(ulc)
     lcidx = find(strcmp(lc, ulc{i}));
     dmae = dMAE_NEE(idx, lcidx);
-    scatter(reshape(dmae,[],1), reshape(repmat(1:ks, length(lcidx), 1)', [],1), 10, clr(i,:), 'filled')
+    scatter(reshape(dmae,[],1), reshape(repmat(1:ks, length(lcidx), 1)'+normrnd(0,stagger,size(dmae)), [],1), 10, clr(i,:), 'filled')
     hold on;
 end
 scatter(ym(idx), 1:ks, 40, 'k', 'Marker','|', 'LineWidth',1.5)
-xlabel('\DeltaMAE (g C m^{-2} day^{-1})')
+xlabel('\DeltaMAE (%)')
 ax = gca;
 box off;
-set(ax, 'TickDir','out', 'TickLength',[0.025 0],'YColor','w','FontSize',9)
-ax.Position(1) = 0.22;
-ax.Position(3) = 0.75;
-text(repmat(ax.XLim(1),1,ks), 1:ks, varlabels(idx), 'HorizontalAlignment','right','FontSize',9)
-lgd = legend('ENF','GRA','SAV','SHB','Median', 'Location','southeast', 'FontSize',9);
-legend('boxoff')
+set(ax, 'TickDir','out', 'TickLength',[0.025 0],'YColor','w','FontSize',9,'XLim',xlim)
+ax.Position(1) = 0.28;
+ax.Position(3) = 0.7;
+text(repmat(ax.XLim(1),1,ks), 1:ks, varlabels(idx), 'HorizontalAlignment','right','FontSize',8)
+text(xlim(2),1,'b', 'FontSize',12, 'HorizontalAlignment','right')
 
-set(gcf,'PaperPositionMode','auto','InvertHardCopy','off')
-print('-dtiff','-f1','-r300','./output/DrylANNd_variableimportance_NEE_monthly.tif')
-close all;
-
-% Plot ET variable importance
-h = figure('Color','w');
-h.Units = 'inches';
-h.Position = [1 1 5 4];
-
+% ET
+xlim = [-25 175];
+axes(axf(3))
 ym = nanmedian(dMAE_ET, 2);
 [~,idx] = sort(ym,'ascend');
 
 for i = 1:length(ulc)
     lcidx = find(strcmp(lc, ulc{i}));
     dmae = dMAE_ET(idx, lcidx);
-    scatter(reshape(dmae,[],1), reshape(repmat(1:ks, length(lcidx), 1)', [],1), 10, clr(i,:), 'filled')
+    scatter(reshape(dmae,[],1), reshape(repmat(1:ks, length(lcidx), 1)'+normrnd(0,stagger,size(dmae)), [],1), 10, clr(i,:), 'filled')
     hold on;
 end
 scatter(ym(idx), 1:ks, 40, 'k', 'Marker','|', 'LineWidth',1.5)
-xlabel('\DeltaMAE (mm day^{-1})')
+xlabel('\DeltaMAE (%)')
 ax = gca;
 box off;
-set(ax, 'TickDir','out', 'TickLength',[0.025 0],'YColor','w','FontSize',9)
-ax.Position(1) = 0.22;
-ax.Position(3) = 0.75;
-text(repmat(ax.XLim(1),1,ks), 1:ks, varlabels(idx), 'HorizontalAlignment','right','FontSize',9)
-lgd = legend('ENF','GRA','SAV','SHB','Median', 'Location','southeast', 'FontSize',9);
-legend('boxoff')
+set(ax, 'TickDir','out', 'TickLength',[0.025 0],'YColor','w','FontSize',9,'XLim',xlim)
+ax.Position(1) = 0.28;
+ax.Position(3) = 0.7;
+text(repmat(ax.XLim(1),1,ks), 1:ks, varlabels(idx), 'HorizontalAlignment','right','FontSize',8)
+text(xlim(2),1,'c', 'FontSize',12, 'HorizontalAlignment','right')
 
 set(gcf,'PaperPositionMode','auto','InvertHardCopy','off')
-print('-dtiff','-f1','-r300','./output/DrylANNd_variableimportance_ET_monthly.tif')
+print('-dtiff','-f1','-r300','./output/DrylANNd_variableimportance_monthly.tif')
+close all;
+
+%% Presentation figure
+h = figure('Color','k');
+h.Units = 'inches';
+h.Position = [1 1 3.5 8];
+
+axf = tight_subplot(3, 1, 0.08, [0.1 0.04], [0.1 0.04]);
+
+% GPP
+xlim = [-25 175];
+axes(axf(1))
+ym = nanmedian(dMAE_GPP, 2);
+[~,idx] = sort(ym,'ascend');
+
+for i = 1:length(ulc)
+    lcidx = find(strcmp(lc, ulc{i}));
+    dmae = dMAE_GPP(idx, lcidx);
+    scatter(reshape(dmae,[],1), reshape(repmat(1:ks, length(lcidx), 1)'+normrnd(0,stagger,size(dmae)), [],1), 10, clr(i,:), 'filled')
+    hold on;
+end
+scatter(ym(idx), 1:ks, 40, 'w', 'Marker','|', 'LineWidth',1.5)
+xlabel('\DeltaMAE (%)', 'Color','w')
+ax = gca;
+box off;
+set(ax, 'TickDir','out', 'TickLength',[0.025 0],'FontSize',9,...
+    'XLim',xlim, 'Color','k', 'XColor','w', 'YColor','k')
+ax.Position(1) = 0.28;
+ax.Position(3) = 0.7;
+text(repmat(ax.XLim(1),1,ks), 1:ks, varlabels(idx), 'HorizontalAlignment','right','FontSize',8,'Color','w')
+lgd = legend('ENF','GRS','SAV','SHB', 'Location','northoutside', 'FontSize',7, 'Orientation','horizontal');
+lgd.Position(2) = 0.97;
+legend('boxoff')
+text(xlim(2),1,'a', 'FontSize',12, 'HorizontalAlignment','right')
+
+% NEE
+xlim = [-25 175];
+axes(axf(2))
+ym = nanmedian(dMAE_NEE, 2);
+[~,idx] = sort(ym,'ascend');
+
+for i = 1:length(ulc)
+    lcidx = find(strcmp(lc, ulc{i}));
+    dmae = dMAE_NEE(idx, lcidx);
+    scatter(reshape(dmae,[],1), reshape(repmat(1:ks, length(lcidx), 1)'+normrnd(0,stagger,size(dmae)), [],1), 10, clr(i,:), 'filled')
+    hold on;
+end
+scatter(ym(idx), 1:ks, 40, 'w', 'Marker','|', 'LineWidth',1.5)
+xlabel('\DeltaMAE (%)', 'Color','w')
+ax = gca;
+box off;
+set(ax, 'TickDir','out', 'TickLength',[0.025 0],'YColor','k','FontSize',9,...
+    'XLim',xlim, 'Color','k', 'XColor','w')
+ax.Position(1) = 0.28;
+ax.Position(3) = 0.7;
+text(repmat(ax.XLim(1),1,ks), 1:ks, varlabels(idx), 'HorizontalAlignment','right','FontSize',8,'Color','w')
+text(xlim(2),1,'b', 'FontSize',12, 'HorizontalAlignment','right')
+
+% ET
+xlim = [-25 175];
+axes(axf(3))
+ym = nanmedian(dMAE_ET, 2);
+[~,idx] = sort(ym,'ascend');
+
+for i = 1:length(ulc)
+    lcidx = find(strcmp(lc, ulc{i}));
+    dmae = dMAE_ET(idx, lcidx);
+    scatter(reshape(dmae,[],1), reshape(repmat(1:ks, length(lcidx), 1)'+normrnd(0,stagger,size(dmae)), [],1), 10, clr(i,:), 'filled')
+    hold on;
+end
+scatter(ym(idx), 1:ks, 40, 'w', 'Marker','|', 'LineWidth',1.5)
+xlabel('\DeltaMAE (%)', 'Color','w')
+ax = gca;
+box off;
+set(ax, 'TickDir','out', 'TickLength',[0.025 0],'YColor','k','FontSize',9,...
+    'XLim',xlim, 'Color','k', 'XColor','w')
+ax.Position(1) = 0.28;
+ax.Position(3) = 0.7;
+text(repmat(ax.XLim(1),1,ks), 1:ks, varlabels(idx), 'HorizontalAlignment','right','FontSize',8,'Color','w')
+text(xlim(2),1,'c', 'FontSize',12, 'HorizontalAlignment','right')
+
+set(gcf,'PaperPositionMode','auto','InvertHardCopy','off')
+print('-dtiff','-f1','-r300','./presentation/DrylANNd_variableimportance_monthly.tif')
 close all;
 
 
