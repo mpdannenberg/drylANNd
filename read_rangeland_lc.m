64cc3d500340b555eb24c992ef8e77498b36eeb0
mpdannenberg <matthew-dannenberg@uiowa.edu>
2022-03-13 15:41:03 -0500

    Revert to static lc

--- read_rangeland_lc.m
+++ read_rangeland_lc.m
@@ -2,36 +2,35 @@
 
 load ./data/Ameriflux_daily.mat;
 n = length(Ameriflux);
-yrs = 2015:2020;
 
-% T = readtable('./data/RangelandAnalysis_FractionalCover/Ameriflux_site_subset_2020VegetationCover.csv');
+T = readtable('./data/Ameriflux_site_subset_2020VegetationCover.csv');
 
 for i = 1:n
     
     site = Ameriflux(i).Site;
-    yr = Ameriflux(i).Year;
-    dt = datenum(yr, 1, 1) + Ameriflux(i).DOY - 1;
+    dt = datenum(Ameriflux(i).Year, 1, 1) + Ameriflux(i).DOY - 1;
 
+    idx = strcmp(T.Site_ID, site);
+    
     Ameriflux(i).Rangeland_AFG = NaN(size(dt));
+    Ameriflux(i).Rangeland_AFG(:) = T.AFG(idx);
+    
     Ameriflux(i).Rangeland_PFG = NaN(size(dt));
+    Ameriflux(i).Rangeland_PFG(:) = T.PFG(idx);
+    
     Ameriflux(i).Rangeland_SHR = NaN(size(dt));
+    Ameriflux(i).Rangeland_SHR(:) = T.SHR(idx);
+    
     Ameriflux(i).Rangeland_TRE = NaN(size(dt));
+    Ameriflux(i).Rangeland_TRE(:) = T.TRE(idx);
+    
     Ameriflux(i).Rangeland_LTR = NaN(size(dt));
+    Ameriflux(i).Rangeland_LTR(:) = T.LTR(idx);
+    
     Ameriflux(i).Rangeland_BGR = NaN(size(dt));
-
-    for y = 1:length(yrs)
-        T = readtable(['./data/RangelandAnalysis_FractionalCover/Ameriflux_sites_', num2str(yrs(y)), '_VegetationCover.xlsx']);
-        
-        idx = strcmp(T.Site_ID, site);
-        
-        Ameriflux(i).Rangeland_AFG(yr == yrs(y)) = T.AFG(idx);
-        Ameriflux(i).Rangeland_PFG(yr == yrs(y)) = T.PFG(idx);
-        Ameriflux(i).Rangeland_SHR(yr == yrs(y)) = T.SHR(idx);
-        Ameriflux(i).Rangeland_TRE(yr == yrs(y)) = T.TRE(idx);
-        Ameriflux(i).Rangeland_LTR(yr == yrs(y)) = T.LTR(idx);
-        Ameriflux(i).Rangeland_BGR(yr == yrs(y)) = T.BGR(idx);
-        
-    end
+    Ameriflux(i).Rangeland_BGR(:) = T.BGR(idx);
+    
+    
 end
 
 save('./data/Ameriflux_daily.mat','Ameriflux');
