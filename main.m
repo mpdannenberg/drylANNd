3b1865bf178f05df7c3048f82ff36aded4c25cc2
mpdannenberg <matthew-dannenberg@uiowa.edu>
2022-03-23 10:51:47 -0500

    Add new scripts and remove obsolete scripts

--- main.m
+++ main.m
@@ -1,8 +1,8 @@
 % Main file for DrylANNd modeling
 
-% Subset TerraClimate aridity index and MODIS LC fraction
+% Subset TerraClimate aridity index and Rangeland Analysis vegetation fraction
 get_terraclimate_aridity;
-get_modis_lc;
+get_rangeland_analysis_gridded;
 
 % Read in and QC-filter Ameriflux data
 read_ameriflux_data;
@@ -13,22 +13,17 @@ read_gridded_mcd43; % Full western U.S.
 
 % Read in MODIS LST
 read_modis_lst; % Site-level
-read_gridded_lst; % Full western U.S.
+read_gridded_mod11; % Full western U.S.
+read_gridded_myd11; % Full western U.S.
 
 % Read in SMAP L4 soil moisture/temperature
 read_smap_l4sm; % Site-level
 read_gridded_sm; % Full western U.S.
 
-% Read in MODIS land cover fraction
-read_modis_lc; % Site-level
+% Read in Rangeland Analysis vegetation fraction
+read_rangeland_lc; % Site-level
 
-% Read in SMAP L3 VOD/vegetation water content - NOT USED FOR NOW
-% read_smap_l3vod;
-
-% Filter out outlier NEE and recalculate GPP - NOT USED FOR NOW
-%filter_ameriflux_data;
-
-% Aggregate daily variables to 8- and 16-day and monthly
+% Aggregate daily variables to 8-day, 16-day, and monthly
 get_8day_ameriflux;
 get_16day_ameriflux;
 get_monthly_ameriflux;
@@ -44,11 +39,6 @@ get_monthly_gridded_lst;
 get_8day_gridded_nbar;
 get_16day_gridded_nbar;
 get_monthly_gridded_nbar;
-
-% Fit DrylANNd model for each temporal interval
-fit_drylannd_8day;
-fit_drylannd_16day;
-fit_drylannd_monthly;
 
 % Cross-validate DrylANNd model on each site (i.e., leave-one-site-out)
 validate_drylannd_8day;
@@ -68,4 +58,16 @@ calculate_variable_importance_monthly;
 %% Figures
 % Make map of Ameriflux sites
 make_ameriflux_map;
+
+% Plots of R^2 (overall and by site)
+plot_r2_8day;
+plot_r2_16day;
+plot_r2_monthly;
+plot_r2_byTimescale;
+
+% Plots of mean seasonal cycles
+plot_seasonality_monthly;
+
+% Plots of (inter)annual variability
+plot_annual_monthly;
 
