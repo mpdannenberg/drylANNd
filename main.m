68e9a05be47d0016288248d71ea8523192154015
mpdannenberg <matthew-dannenberg@uiowa.edu>
2022-08-31 09:55:58 -0500

    Update file names

--- main.m
+++ main.m
@@ -2,7 +2,7 @@
 
 % Subset TerraClimate aridity index and Rangeland Analysis vegetation fraction
 get_terraclimate_aridity;
-get_rangeland_analysis_gridded;
+get_rangeland_analysis;
 
 % Read in and QC-filter Ameriflux data
 read_ameriflux_data;
@@ -24,50 +24,39 @@ read_gridded_sm; % Full western U.S.
 read_rangeland_lc; % Site-level
 
 % Aggregate daily variables to 8-day, 16-day, and monthly
-get_8day_ameriflux;
-get_16day_ameriflux;
 get_monthly_ameriflux;
-
-get_8day_gridded_smap;
-get_16day_gridded_smap;
 get_monthly_gridded_smap;
-
-get_8day_gridded_lst;
-get_16day_gridded_lst;
 get_monthly_gridded_lst;
-
-get_8day_gridded_nbar;
-get_16day_gridded_nbar;
 get_monthly_gridded_nbar;
 
 % Cross-validate DrylANNd model on each site (i.e., leave-one-site-out)
-validate_drylannd_8day;
-validate_drylannd_16day;
 validate_drylannd_monthly;
 
 % Predict GPP, NEE, and ET with calibrated DrylANNd model
-predict_drylannd_8day;
-predict_drylannd_16day;
 predict_drylannd_monthly;
+write_drylannd_netcdf_monthly;
 
 % Test variable importance
-calculate_variable_importance_8day;
-calculate_variable_importance_16day;
 calculate_variable_importance_monthly;
+calculate_variable_importance_byVariableClass;
 
 %% Figures
 % Make map of Ameriflux sites
 make_ameriflux_map;
 
 % Plots of R^2 (overall and by site)
-plot_r2_8day;
-plot_r2_16day;
 plot_r2_monthly;
-plot_r2_byTimescale;
+
+% Map of mean DrylANNd GPP, NEE, and ET (w/ scatterplot comparison to
+% AmeriFlux
+map_drylannd_mean;
 
 % Plots of mean seasonal cycles
-plot_seasonality_monthly;
+plot_seasonality;
 
 % Plots of (inter)annual variability
-plot_annual_monthly;
+plot_annual;
+
+% Plots of variable importance
+plot_variable_importance_byVariableClass;
 
